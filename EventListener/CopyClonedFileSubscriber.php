80e82f1b5a540ae92a22196e6dd1bcecfe44300e
Igor Nikolaev <igor.sv.n@gmail.com>
2020-11-18 16:24:34 +0300

    Add copy cloned file event subscriber.

---
+++ EventListener/CopyClonedFileSubscriber.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2017-2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\FileBundle\EventListener;
+
+use Darvin\FileBundle\Entity\AbstractFile;
+use Darvin\Utils\Event\ClonableEvents;
+use Darvin\Utils\Event\CloneEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Copy cloned file event subscriber
+ */
+class CopyClonedFileSubscriber implements EventSubscriberInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ClonableEvents::CLONED => ['copyFile', -10],
+        ];
+    }
+
+    /**
+     * @param \Darvin\Utils\Event\CloneEvent $event Event
+     */
+    public function copyFile(CloneEvent $event): void
+    {
+        $original = $event->getOriginal();
+
+        if (!$original instanceof AbstractFile) {
+            return;
+        }
+
+        /** @var \Darvin\FileBundle\Entity\AbstractFile $clone */
+        $clone = $event->getClone();
+
+        if (null === $clone->getFile()) {
+            $event->setClone(null);
+
+            return;
+        }
+
+        $clone->setName(null);
+    }
+}
