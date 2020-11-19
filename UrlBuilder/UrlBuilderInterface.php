30b9fb0b914dbe80bb3fb5148fad53954450bd4c
Igor Nikolaev <igor.sv.n@gmail.com>
2020-11-19 08:35:26 +0300

    Add URL builder interface.

---
+++ UrlBuilder/UrlBuilderInterface.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Igor Nikolaev <igor.sv.n@gmail.com>
+ * @copyright Copyright (c) 2015-2019, Darvin Studio
+ * @link      https://www.darvin-studio.ru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Darvin\FileBundle\UrlBuilder;
+
+use Darvin\FileBundle\Entity\AbstractFile;
+
+/**
+ * URL builder
+ */
+interface UrlBuilderInterface
+{
+    /**
+     * @param \Darvin\FileBundle\Entity\AbstractFile|null $file        File
+     * @param bool                                        $prependHost Whether to prepend host
+     *
+     * @return string|null
+     */
+    public function buildOriginalUrl(?AbstractFile $file, bool $prependHost = false): ?string;
+
+    /**
+     * @param \Darvin\FileBundle\Entity\AbstractFile|null $file File
+     *
+     * @return bool
+     */
+    public function isActive(?AbstractFile $file): bool;
+}
